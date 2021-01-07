7f1519e671e34f0a0906b0585e13b79a29955efe
Anshit Verma <anshitverma55@gmail.com>
2021-01-07 16:27:02 +0530

    added styles

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +28,7 @@ urlpatterns = [
     path("thanks/", views.ThanksPage.as_view(), name="thanks"),
     path("posts/", include('posts.urls', namespace='posts')),
     path("groups/", include('groups.urls', namespace='groups'))
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
